a5e447613280df9df8305a5b863ce54cdb38112f
Ben Winston <ben@benwinston.us>
2020-07-02 22:22:34 -0400

    make the search function more like mpc

--- nq.sh
+++ nq.sh
@@ -22,7 +22,23 @@ add() {
 }
 
 search() {
-    grep -i "$1" $all_tracks | awk -F';;' '{ print substr($1,1,8) "  " $4 "/" $3 "/" $5 " - " $6 }' | sort -k 2
+    if [[ -z "$2" ]]; then
+        printf %s\\n "missing a search term!"
+	return 1
+    fi
+    case "$1" in
+        artist)
+    	    artist_list=`cat $all_tracks | awk -F';;' '{ print ";;" $4 ";;" }' | sort | uniq | grep -i "$2"`
+            IFS=$'\n'
+            for artist in $artist_list; do
+                grep "$artist" $all_tracks | awk -F';;' '{ print substr($1,1,8) "  " $4 "/" $3 "/" $5 " - " $6 }' | sort -k 2
+            done
+	    unset IFS
+	    ;;
+        *)
+            printf %s\\n "not recognized"
+	    ;;
+    esac 
 }
 
 case "$1" in
@@ -36,7 +52,7 @@ case "$1" in
         toggle
         ;;
     search)
-        search "$2"
+        search "$2" "$3"
         ;;
     add)
         add
